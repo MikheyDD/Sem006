cc3ab6161f3a327fc66ec272033eb91229e9b71e
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-29 21:28:06 +0300

    Sem006

---
+++ task044/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+
+// 5 => 0 1 1 2 3 
+PrintFnibancciNum(10);
+void PrintFnibancciNum(int count)
+{
+    int currentNum = 0;
+    int currentCount = 2;
+    int firstNum = 0;
+    int secondNum = 1;
+    Console.Write("0, 1, ");
+    while (currentCount != count)
+    {
+        currentNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = currentNum;
+        Console.Write($"{currentNum}, ");
+        currentCount++;
+
+    }
+}
